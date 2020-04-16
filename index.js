321a647a7d28f3b93f5645629be3b38e4c16d2c7
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-04-16 09:32:12 -0600

    updates

--- index.js
+++ index.js
@@ -1,12 +1,92 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-function writeToFile(fileName, data) {
+function promptUser() {
+
+    return inquirer.prompt([
+
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+        },
+
+        {
+            type: "input",
+            name: "location",
+            message: "Where are you from?",
+        },
+
+        {
+            type: "input",
+            name: "hobby",
+            message: "What is your favorite hobby?",
+
+        },
+
+        {
+            type: "input",
+            name: "food",
+            message: "What is your favorite food?",
+
+        },
+
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your Github username",
+
+        },
+
+        {
+            type: "input",
+            name: "linkedin",
+            message: "Enter your LinkedIn URL",
+
+        },
+
+
+    ]);
+
 }
 
-function init() {
+async function init() {
 
+    console.log("Hey there!")
+
+    try {
+
+        const answers = await promptUser();
+
+        const html = generateHTML(answers);
+        
+        await writeFileAsync("index.html", html);
+
+        console.log("Successfully written to index.html");
+
+    } catch(err) {
+
+        console.log("Error");
+    }
 }
 
-init();
+
+
+
+
+
+// const questions = [
+
+// ];
+
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
