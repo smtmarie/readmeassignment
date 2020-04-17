495b9d0c756273384eb8d7d0960519e00cb42383
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-04-16 18:11:50 -0600

    continued fun

--- index.js
+++ index.js
@@ -5,51 +5,48 @@ const path = require("path");
 
 const questions = [
 
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+    },
 
-        {
-            type: "input",
-            name: "name",
-            message: "What is your name?",
-        },
+    {
+        type: "input",
+        name: "location",
+        message: "Where are you from?",
+    },
 
-        {
-            type: "input",
-            name: "location",
-            message: "Where are you from?",
-        },
+    {
+        type: "input",
+        name: "hobby",
+        message: "What is your favorite hobby?",
 
-        {
-            type: "input",
-            name: "hobby",
-            message: "What is your favorite hobby?",
+    },
 
-        },
+    {
+        type: "input",
+        name: "food",
+        message: "What is your favorite food?",
 
-        {
-            type: "input",
-            name: "food",
-            message: "What is your favorite food?",
+    },
 
-        },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your Github username",
 
-        {
-            type: "input",
-            name: "github",
-            message: "Enter your Github username",
+    },
 
-        },
+    {
+        type: "input",
+        name: "linkedin",
+        message: "Enter your LinkedIn URL",
 
-        {
-            type: "input",
-            name: "linkedin",
-            message: "Enter your LinkedIn URL",
-
-        },
+    },
 
 
-    ]);
-
-}
+]
 
 async function init() {
 
@@ -60,12 +57,12 @@ async function init() {
         const answers = await promptUser();
 
         const html = generateHTML(answers);
-        
+
         await writeFileAsync("index.html", html);
 
         console.log("Successfully written to index.html");
 
-    } catch(err) {
+    } catch (err) {
 
         console.log("Error");
     }
